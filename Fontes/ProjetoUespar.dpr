c2295de3ec9f279df03c3cff8c3e8b0b637b0e40
DiegoJahn <diego_jahn@outlook.com>
2017-08-18 19:15:28 -0300

    teste
    Signed-off-by: DiegoJahn <diego_jahn@outlook.com>

--- Fontes/ProjetoUespar.dpr
+++ Fontes/ProjetoUespar.dpr
@@ -6,17 +6,22 @@ uses
   U_Cidade in 'U_Cidade.pas' {F_Estado},
   U_Estado in 'U_Estado.pas' {F_Cidade},
   U_Cliente in 'U_Cliente.pas' {F_Cliente},
-  U_Principal in 'U_Principal.pas' {F_Principal};
+  U_Principal in 'U_Principal.pas' {F_Principal},
+  U_Produtos in 'U_Produtos.pas' {F_Produtos},
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Carbon');
   Application.CreateForm(TF_Principal, F_Principal);
   Application.CreateForm(TF_Modelo, F_Modelo);
   Application.CreateForm(TF_Estado, F_Estado);
   Application.CreateForm(TF_Cidade, F_Cidade);
   Application.CreateForm(TF_Cliente, F_Cliente);
+  Application.CreateForm(TF_Produtos, F_Produtos);
   Application.Run;
 end.
